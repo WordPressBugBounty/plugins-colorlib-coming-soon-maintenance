e5bc79d4a56860bbb67985e0ca8c443dce619991
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 14:26:32 +0000

    1.2.0

--- colorlib-coming-soon-maintenance/includes/class-ccsm-review.php
+++ colorlib-coming-soon-maintenance/includes/class-ccsm-review.php
@@ -10,7 +10,7 @@ class CCSM_Review {
 	private $slug = '';
 	private $option_name = '';
 
-	function __construct( $args ) {
+	public function __construct( $args ) {
 
 		if ( isset( $args['slug'] ) ) {
 			$this->slug = $args['slug'];
@@ -60,24 +60,24 @@ class CCSM_Review {
 
 		$options = get_option( 'ccsm_settings' );
 		$option  = isset( $options['givemereview'] ) ? $options['givemereview'] : '';
-		$currDate = date('Y-m-d');
-		if ( 'already-rated' == $option ) {
+
+		if ( 'already-rated' === $option ) {
 			return false;
 		}
 
-		if ( $this->value == $option && "" != $option ) {
+		if ( $this->value === $option && '' !== $option ) {
 			return false;
 		}
 
 		if ( is_array( $this->when ) ) {
-			foreach($this->when as $et){
-                if ($et == $this->value) {
-                    return true;
-                }
-
+			foreach ( $this->when as $et ) {
+				if ( (int) $et === (int) $this->value ) {
+					return true;
+				}
 			}
 		}
 
+		return false;
 	}
 
 	private function value() {
@@ -91,8 +91,10 @@ class CCSM_Review {
             return round($date_diff / (60 * 60 * 24));
 		}
 
-		$date = date( 'Y-m-d' );
+		$date = gmdate( 'Y-m-d' );
 		set_transient( 'ccsm_review', $date, 24 * 30 * HOUR_IN_SECONDS );
+
+		return 0;
 
 	}
 
@@ -102,7 +104,7 @@ class CCSM_Review {
 
 		?>
         <div id="<?php echo esc_attr($this->slug); ?>-epsilon-review-notice" class="notice notice-success is-dismissible">
-            <p><?php echo sprintf( wp_kses_post( $this->messages['notice'] ), wp_kses_post( $this->value ) ); ?></p>
+            <p><?php echo wp_kses_post( sprintf( $this->messages['notice'], $this->value ) ); ?></p>
             <p class="actions">
                 <a id="epsilon-rate" href="<?php echo esc_url( $url ) ?>"
                    class="button button-primary epsilon-review-button"><?php echo esc_html( $this->messages['rate'] ); ?></a>
@@ -119,10 +121,14 @@ class CCSM_Review {
 
 		check_ajax_referer( 'epsilon-review', 'security' );
 
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( -1 );
+		}
+
 		$options = get_option( 'ccsm_settings', array() );
 
-		if ( isset( $_POST['epsilon-review'] ) ) {
-			$options['givemereview'] = "already-rated";
+		if ( isset( $_POST['epsilon-review'] ) ) { // phpcs:ignore WordPress.Security.ValidatedSanitizedInput
+			$options['givemereview'] = 'already-rated';
 		} else {
 			$options['givemereview'] = $this->value;
 		}
